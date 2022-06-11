1ad748edb1f5f4203a9ca5097d1742e8b265759a
dkotama <darma.kotama@gmail.com>
2022-06-11 20:10:20 +0800

    Commenting steps

--- src/main/java/com/instiki/pertemuan13swingform/Main.java
+++ src/main/java/com/instiki/pertemuan13swingform/Main.java
@@ -15,11 +15,11 @@ public class Main {
 
     public static void main(String[] args) {
      //Creating object of LoginFrame class and setting some of its properties
-        LoginFrame frame=new LoginFrame();
-        frame.setTitle("Login Form");
-        frame.setVisible(true);
-        frame.setBounds(10,10,370,600);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
+//        LoginFrame frame=new LoginFrame();
+//        frame.setTitle("Login Form");
+//        frame.setVisible(true);
+//        frame.setBounds(10,10,370,600);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setResizable(false);
     }
 }
